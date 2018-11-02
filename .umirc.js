dfc191c2606f3bf32829d99d4c62426e767c261d
chencheng <sorrycc@gmail.com>
2018-11-02 16:38:54 +0800

    :tada: First commit

---
+++ .umirc.js
@@ -0,0 +1,19 @@
+
+// ref: https://umijs.org/config/
+export default {
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: false,
+      dva: false,
+      dynamicImport: false,
+      title: 'umi-example-lighthouse',
+      pwa: {},
+      dll: false,
+      routes: {
+        exclude: [],
+      },
+      hardSource: false,
+    }],
+  ],
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# umi-example-lighthouse
+
+<img src="https://gw.alipayobjects.com/zos/rmsportal/lIxlKcdyRnsBhbzVGwiR.png" />
+
